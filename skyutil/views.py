5a2ce2a4566268f3f3e27689f543d894c3ea998b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-28 13:47:30 -0700

    Merge create and update view methods

--- skyutil/views.py
+++ skyutil/views.py
@@ -1,14 +1,17 @@
 import os
 import pyarrow.dataset as pa_ds
 
+
 VIEWS_DIR = "/mnt/cephfs/view"
 
-def create_view(table, view_name, **kwargs):
+
+def create_view(table, view_name, update=False, **kwargs):
     if not os.path.exists(VIEWS_DIR):
         os.makedirs(VIEWS_DIR)
 
     if os.path.exists(os.path.join(VIEWS_DIR, view_name)):
-        raise Exception("View {} already exists.".format(view_name))
+        if not update:
+            raise Exception("View {} already exists.".format(view_name))
 
     return pa_ds.write_dataset(table, os.path.join(VIEWS_DIR, view_name), **kwargs)
 
@@ -19,7 +22,7 @@ def delete_view(view_name):
     os.remove(os.path.join(VIEWS_DIR, view_name))
 
 
-def view(view_name, **kwargs):
+def get_view(view_name, **kwargs):
     if not os.path.exists(os.path.join(VIEWS_DIR, view_name)):
         raise Exception("View {} does not exist".format(view_name))
     return pa_ds.dataset(os.path.join(VIEWS_DIR, view_name), format="parquet", **kwargs)
