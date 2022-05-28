81c1899969d51c3e79263130a647f164577e5ca0
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-28 14:12:03 -0700

    fix setup.py

--- examples/1.py
+++ examples/1.py
@@ -2,5 +2,5 @@ from skyutil import LazyView
 
 if __name__ == "__main__":
     query = ('total_amount', '>', 100)
-    view = LazyView("/tmp/views", "sample.parquet")
+    view = LazyView("/tmp/views", "sample.prquet")
     
--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from skyutil.version import Version
 setuptools.setup(name='skyutil',
                  version=Version('1.0.0').number,
                  description='Misc. skyhook utilites',
-                 long_description=open('README.md').read().strip(),
                  author='Jayjeet Chakraborty',
                  author_email='jayjeetc@ucsc.edu',
                  py_modules=['skyutil'],
